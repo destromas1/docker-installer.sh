765ef9af92c271fe6500a6e94f2223f72b5f1529
Shahjada <shahjada.talukdar@gmail.com>
2016-10-29 14:09:46 +0600

    docker-installer.sh for Ubuntu 14.04

---
+++ docker-installer.sh
@@ -0,0 +1,26 @@
+# Its For  Ubuntu
+
+uname -r
+
+sudo apt-get update
+
+sudo apt-get install apt-transport-https ca-certificates
+
+sudo apt-key adv --keyserver hkp://p80.pool.sks-keyservers.net:80 --recv-keys 58118E89F3A912897C070ADBF76221572C52609D
+
+sudo su -c echo "deb https://apt.dockerproject.org/repo ubuntu-trusty main" > /etc/apt/sources.list.d/docker.list
+
+sudo apt-get update
+
+apt-cache policy docker-engine
+
+sudo apt-get update
+
+sudo apt-get install linux-image-extra-$(uname -r) linux-image-extra-virtual
+
+sudo apt-get update
+
+sudo apt-get install docker-engine
+
+sudo service docker start
+
